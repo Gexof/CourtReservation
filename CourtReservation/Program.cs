b630e1e367d6960cdfdcfda76ae777e6ea56b166
Mohamed Ashraf <gexof01@gmail.com>
2024-01-01 02:03:40 +0200

    Welcome Message and login options

--- CourtReservation/Program.cs
+++ CourtReservation/Program.cs
@@ -4,6 +4,19 @@
     {
         static void Main(string[] args)
         {
+            
+            Console.Title = ("Court Reservation");
+
+            Console.WriteLine("=============================");
+            Console.WriteLine("Welcome To Court Reservation");
+            Console.WriteLine("============================= \n");
+
+            Console.WriteLine("Please Select an Option Register or Login");
+            Console.WriteLine("[1] Register");
+            Console.WriteLine("[2] Login");
+
+
+
 
         }
     }
