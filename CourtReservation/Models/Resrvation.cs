e05ffb63bb84541540c836770e064ce4f10868d4
YASMIENYASSER <155325517+YASMIENYASSER@users.noreply.github.com>
2024-01-01 17:57:23 +0200

    Reservation class

--- CourtReservation/Models/Resrvation.cs
+++ CourtReservation/Models/Resrvation.cs
@@ -6,8 +6,35 @@ using System.Threading.Tasks;
 
 namespace CourtReservation.Models
 {
-    internal class Resrvation
+    internal class Reservation:Court,User
     {
+        int resrvationid { set; get; }
+        dateOnly date { set; get; }
+        public void show_reservation_details()
+        {
+            Console.WriteLine(resrvationid);
+            Console.WriteLine(username);
+            Console.WriteLine(courttype);
+            Console.WriteLine(date);
+        }
+        public void cancel_reservation()
+        {
+
+        }
+        public Reservation(int resrvationid, string username,string courttype,dateOnly date)
+            :base(username,type)
+        {
+            this.resrvationid = resrvationid;
+            this.date = date;
+        }
+
+
+
+
+
+
 
     }
+
+
 }
