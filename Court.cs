342c616697be55f3834549ce86aac13ea69e3cc9
OmarrAhmedd <111094934+OmarrAhmedd@users.noreply.github.com>
2024-01-01 02:15:47 +0200

    SSS

---
+++ Court.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Court
+{
+	public Court()
+	{
+		public int CourtId { get; set; }
+
+    public string Description { get; set; }
+
+    public String Type { get; set; }
+
+	public abstract string ShowCollection { get; set; };
+
+}
+}
