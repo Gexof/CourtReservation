d7244c230ac25b9929f929a62cff8d7eec1a18e8
Islam Elsayed <islamshawqi525@gmail.com>
2024-01-01 02:13:33 +0200

    Create User > Customer

--- CourtReservation/Program.cs
+++ CourtReservation/Program.cs
@@ -14,7 +14,10 @@
             Console.WriteLine("Please Select an Option Register or Login");
             Console.WriteLine("[1] Register");
             Console.WriteLine("[2] Login");
-            Console.WriteLine("[2] Login");
+
+            string[] User = new string[] { "Customer", "Admin" };
+            Customer customer = new Customer(7, "Islam", 1234, "Court4");
+            customer.PrintUserData();
 
 
 
---
+++ CourtReservation/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourtReservation
+{
+    internal class User
+    {
+        //Attribute
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public int Password { get; set; }
+
+
+        //Default Constructor
+        public User()
+        {
+            Id = 0;
+            UserName = "";
+            Password = 0;
+        }
+
+        //Parametarized Constructor
+        public User(int Id, string UserName, int Password)
+        {
+            this.Id = Id;
+            this.UserName = UserName;
+            this.Password = Password;
+
+        }
+
+        public User(string UserName, int Password) : this(100, UserName, Password) { }
+
+
+        //Method
+        public virtual void PrintUserData()
+        {
+            Console.WriteLine("Id is" + this.Id);
+            Console.WriteLine("UserName is" + this.UserName);
+            Console.WriteLine("Password is" + this.Password);
+
+
+        }
+    }
+}
