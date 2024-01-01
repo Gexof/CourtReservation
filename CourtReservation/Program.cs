3da3d840f27747c431ede7185c75bdfa4cddab75
Mohamed Ashraf <gexof01@gmail.com>
2024-01-01 03:11:34 +0200

    Register Screen

--- CourtReservation/Program.cs
+++ CourtReservation/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace CourtReservation
+﻿using Microsoft.Win32;
+
+namespace CourtReservation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             AppScreen.Weclome();
+            int userChosie = int.Parse(Console.ReadLine());
+            if (userChosie == 1)
+            {
+                Console.Clear();
+                RegisterScreen.Register();
+            }
         }
     }
 }
---
+++ CourtReservation/RegisterScreen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourtReservation
+{
+    internal class RegisterScreen
+    {
+        public static void Register () {
+            Console.WriteLine("=================== Register =================== \n");
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+            Console.Write("\nPassowrd: ");
+            string password = Console.ReadLine();
+        }
+    }
+}
